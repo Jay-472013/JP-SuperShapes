493b09341aaddcec22c65d7d6554d7e97c016519
AnthonyTomas2011 <130806226+MinnieMia@users.noreply.github.com>
2024-03-09 12:29:39 -0600

    pg 134 step 26

--- JP-SuperShapes/Assets/Scripts/Shape.cs
+++ JP-SuperShapes/Assets/Scripts/Shape.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Shape : MonoBehaviour
 {
+    [Header("Rigidbody Object")]
+    public Rigidbody rb;
+    [Header("Default Shrinking Speed")]
+    public float ShrinkingSpeed = 3f;
     // Start is called before the first frame update
     void Start()
     {
