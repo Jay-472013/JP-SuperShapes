e82f0c8bfc032136ece1136fff595ddd160fe73f
AnthonyTomas2011 <130806226+MinnieMia@users.noreply.github.com>
2024-03-16 12:34:16 -0500

    Shapey

--- JP-SuperShapes/Assets/Scripts/GameController.cs
+++ JP-SuperShapes/Assets/Scripts/GameController.cs
@@ -5,15 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    [Header("Shape Objects")]
+    public GameObject[] shapePrefabs;
+
+    [Header("Default Spawn Delay Time")]
+    public float spawnDelay = 2f;
+    [Header("Default Spawn Time")]
+    public float spawnTime = 3f;
     // Start is called before the first frame update
     void Start()
     {
-
+        InvokeRepeating("Spawn", spawnDelay, spawnTime);
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+    void Spawn()
+    {
+        int randomInt = Random.Range(0, shapePrefabs.Length);
+
+        Instantiate(shapePrefabs[randomInt], Vector3.zero, Quaternion.identity);
+    }
+    public void GameObject()
+    {
+        CancelInvoke("Spawn");
     }
 }
